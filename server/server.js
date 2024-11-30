6c4e50167a765f7572adc956860ca9f5771608d3
Mia <miaskyelena@gmail.com>
2024-11-29 19:17:17 -0500

    Dotenv configurations

--- server/server.js
+++ server/server.js
@@ -2,8 +2,6 @@ import express from 'express'
 import productsRouter from './routes/products.js'
 
 const app = express() 
-<<<<<<< Updated upstream
-=======
 const port = process.env.PORT || 3001
 
 app.use('/public', express.static('public'))
@@ -15,7 +13,6 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
->>>>>>> Stashed changes
 
 app.use('/public', express.static('./public')) 
 
